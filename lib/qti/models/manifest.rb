783b3dd921933a67a9645271827765481e6d00cb
Hannah Bottalla <hannah@instructure.com>
2017-03-16 13:46:43 -0600

    Fix 2.x manifest search
    closes: QUIZ-1363
    
    test plan:
    QA-CR
    
    Change-Id: I0ad71a29440ee23153ebb32da848b6e2f9904802
    Reviewed-on: https://gerrit.instructure.com/105400
    Tested-by: Jenkins
    Reviewed-by: Ryan Taylor <rtaylor@instructure.com>
    QA-Review: Michael Hargiss <mhargiss@instructure.com>
    Product-Review: Hannah Bottalla <hannah@instructure.com>

--- lib/qti/models/manifest.rb
+++ lib/qti/models/manifest.rb
@@ -9,16 +9,12 @@ module Qti
       end
 
       def qti_1_href
-        xpath_with_single_check(
-          "//xmlns:resources/xmlns:resource[@type='imsqti_xmlv1p1']/@href"
-        )&.content
+        xpath_with_single_check("//xmlns:resources/xmlns:resource[@type='imsqti_xmlv1p1']/@href")&.content
       end
 
       def qti_2_x_href
-        xpath_with_single_check(
-          "//xmlns:resources/xmlns:schemaLocation[@type='imsqti_item_xmlv2p1'
-          or @type='imsqti_test_xmlv2p2']/@href"
-        )&.content
+        xpath_with_single_check("//xmlns:resources/xmlns:resource[@type='imsqti_test_xmlv2p1']/@href")&.content ||
+        xpath_with_single_check("//xmlns:resources/xmlns:resource[@type='imsqti_test_xmlv2p2']/@href")&.content
       end
     end
   end
