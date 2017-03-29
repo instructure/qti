3461430320c61521e47647bf2bef232c8d30ae49
John Corrigan <jcorrigan@instructure.com>
2017-03-29 16:03:33 -0500

    point to v1.2. not v.1.1
    test plan
    - QA-CR
    
    Change-Id: I875f1f093e01413af63f8b02dd2bee9f6e95a998
    Reviewed-on: https://gerrit.instructure.com/106862
    Tested-by: Jenkins
    Reviewed-by: Ryan Taylor <rtaylor@instructure.com>
    QA-Review: Michael Hargiss <mhargiss@instructure.com>
    Product-Review: John Corrigan <jcorrigan@instructure.com>

--- lib/qti/models/manifest.rb
+++ lib/qti/models/manifest.rb
@@ -9,7 +9,7 @@ module Qti
       end
 
       def qti_1_href
-        xpath_with_single_check("//xmlns:resources/xmlns:resource[@type='imsqti_xmlv1p1']/@href")&.content
+        xpath_with_single_check("//xmlns:resources/xmlns:resource[@type='imsqti_xmlv1p2']/@href")&.content
       end
 
       def qti_2_x_href
