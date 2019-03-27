d1f02bdc02af942a267e5e734431053d3c80701e
Steve Kacsmark <skacsmark@instructure.com>
2019-03-27 12:37:05 -0500

    Use item meta data to determine interaction type
    refs QUIZ-6190
    
    Test Plan
     - Test with associated quiz_api patch set
     - Import the QTI file attached to the ticket, it should import
       all three interactions.
     - Import a reference quiz with all interaction types.
     - It should import correctly
    
    Change-Id: I3148cc0ebcb9fd8964133c7951b56cf3b327b278
    Reviewed-on: https://gerrit.instructure.com/187110
    Tested-by: Jenkins
    Reviewed-by: Hannah Bottalla <hannah@instructure.com>
    QA-Review: Hannah Bottalla <hannah@instructure.com>
    Product-Review: Stephen Kacsmark <skacsmark@instructure.com>

--- lib/qti/v1/models/interactions.rb
+++ lib/qti/v1/models/interactions.rb
@@ -9,8 +9,28 @@ module Qti
         ALL_CLASSES = constants.map { |c| const_get(c) }.freeze
         ORDERED_CLASSES = [CanvasMultipleDropdownInteraction, FormulaInteraction, NumericInteraction].freeze
         FALLBACK_CLASSES = [StringInteraction].freeze
+        IDENTIFIED_CLASSES = {
+          'multiple_choice_question' => ChoiceInteraction,
+          'true_false_question' => ChoiceInteraction,
+          'short_answer_question' => FillBlankInteraction,
+          'fill_in_multiple_blanks_question' => FillBlankInteraction,
+          'multiple_answers_question' => ChoiceInteraction,
+          'multiple_dropdowns_question' => CanvasMultipleDropdownInteraction,
+          'matching_question' => MatchInteraction,
+          'numerical_question' => NumericInteraction,
+          'calculated_question' => FormulaInteraction,
+          'essay_question ' => StringInteraction,
+          'file_upload_question' => UploadInteraction
+          # "text_only_question" => StimulusNoQuestion
+        }.freeze
+
         # This one finds the correct parsing model based on the provided xml node
+
         def self.interaction_model(node, parent)
+          matched_class(node, parent) || searched_class(node, parent)
+        end
+
+        def self.searched_class(node, parent)
           matches = Interactions.get_matches(node, parent, ORDERED_CLASSES)
           return matches.first unless matches.empty?
 
@@ -21,6 +41,22 @@ module Qti
 
           raise UnsupportedSchema, "Multiple Types (#{matches.map(&:class)})" if matches.size != 1
           matches.first
+        end
+
+        def self.matched_class(node, parent)
+          IDENTIFIED_CLASSES[question_type(node)]&.new(node, parent)
+        end
+
+        def self.question_type(node)
+          path = './/xmlns:qtimetadatafield/xmlns:fieldlabel' \
+            '[text()="question_type"]/../xmlns:fieldentry'
+          node.at_xpath(path)&.text
+        end
+
+        def self.get_match(node, parent, classlist)
+          classlist.each do |interaction_class|
+            return true if interaction_class.match(node, parent)
+          end
         end
 
         def self.get_matches(node, parent, classlist)
---
+++ spec/lib/qti/v1/models/interactions_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe Qti::V1::Models::Assessment do
+  describe 'interaction item' do
+    let(:fixtures_path) { File.join('spec', 'fixtures') }
+    shared_examples_for 'verify interactions' do
+      it 'has the correct iinteraction type' do
+        loaded_class.assessment_items.zip(expected_item_data).each do |node, itype|
+          item = Qti::V1::Models::AssessmentItem.new(node)
+          expect(item.interaction_model.class).to eq(itype)
+        end
+      end
+    end
+
+    context 'reference qti 1.2 quiz' do
+      let(:path) { File.join(fixtures_path, 'all_canvas_simple_1.2.xml') }
+      let(:loaded_class) { described_class.from_path!(path) }
+      let(:expected_item_data) do
+        [
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::FillBlankInteraction,
+          Qti::V1::Models::Interactions::FillBlankInteraction,
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::CanvasMultipleDropdownInteraction,
+          Qti::V1::Models::Interactions::MatchInteraction,
+          Qti::V1::Models::Interactions::NumericInteraction,
+          Qti::V1::Models::Interactions::FormulaInteraction,
+          Qti::V1::Models::Interactions::StringInteraction
+        ]
+      end
+      include_examples('verify interactions')
+    end
+
+    context 'reference qti 1.2 quiz with no metadata' do
+      let(:path) { File.join(fixtures_path, 'interaction_checks_1.2.xml') }
+      let(:loaded_class) { described_class.from_path!(path) }
+      let(:expected_item_data) do
+        [
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::FillBlankInteraction,
+          Qti::V1::Models::Interactions::MatchInteraction,
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::MatchInteraction,
+          Qti::V1::Models::Interactions::NumericInteraction,
+          Qti::V1::Models::Interactions::FormulaInteraction,
+          Qti::V1::Models::Interactions::StringInteraction
+        ]
+      end
+      include_examples('verify interactions')
+    end
+
+    context 'qti 1.2 edge cases' do
+      let(:path) { File.join(fixtures_path, 'edge_cases_1.2.xml') }
+      let(:loaded_class) { described_class.from_path!(path) }
+      let(:expected_item_data) do
+        [
+          Qti::V1::Models::Interactions::MatchInteraction,
+          Qti::V1::Models::Interactions::ChoiceInteraction,
+          Qti::V1::Models::Interactions::MatchInteraction,
+          Qti::V1::Models::Interactions::ChoiceInteraction
+        ]
+      end
+      include_examples('verify interactions')
+    end
+  end
+end
